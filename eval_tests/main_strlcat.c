953ac6fbb449da4dd6da32f1fa3a76aaeb6b5885
PatrikKarkkainen <karkkainenpatrik@gmail.com>
2021-11-03 19:28:49 +0200

    Added strlcat

--- eval_tests/main_strlcat.c
+++ eval_tests/main_strlcat.c
@@ -10,4 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <stdio.h>
+#include <string.h>
+#include <bsd/string.h>
 
+int main()
+{
+    char first[] = "This is ";
+    char last[] = "a potentially long string";
+    int r;
+    int size = 60;
+    char buffer[size];
+
+    strcpy(buffer,first);
+    r = ft_strlcat(buffer,last,size);
+
+    puts(buffer);
+    printf("Value returned: %d\n",r);
+    if( r > size )
+        puts("String truncated");
+    else
+        puts("String was fully copied");
+
+    return(0);
+}
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -10,4 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	len;
+	size_t	src_size;
+	size_t	dst_size;
+
+	i = 0;
+	src_size = ft_strlen(src);
+	dst_size = ft_strlen(dst);
+	len = dst_size;
+	if (size == 0 || size < dst_size)
+		return (src_size + dst_size);
+	else
+	{
+		while (src[i] && dst_size + i < size - 1)
+		{
+			dst[len] = src[i];
+			i++;
+			len++;
+		}
+		dst[len] = '\0';
+	}
+	
+	return (src_size + dst_size);
+}
