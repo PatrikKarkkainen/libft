9a6b10e1561db4301a3ce8789be7f6018170314e
Patrik Karkkainen <pkarkkai@student.hive.fi>
2021-11-05 13:16:39 +0200

    Added memchr and empty files for next functions

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pkarkkai <pkarkkai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 11:33:24 by pkarkkai          #+#    #+#             */
-/*   Updated: 2021/11/05 12:23:09 by pkarkkai         ###   ########.fr       */
+/*   Updated: 2021/11/05 13:07:13 by pkarkkai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *str1, const void *str2, size_t n);
+void	*ft_memchr(const void *str, int c, size_t n);
 size_t	ft_strlen(const char *str);
 char	*ft_strdup(const char *string);
 char	*ft_strcpy(char *dest, const char *src);
