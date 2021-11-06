51cb1d0f027e7c05401cc1f901bccacb489560a2
PatrikKarkkainen <karkkainenpatrik@gmail.com>
2021-11-06 10:56:51 +0200

    Added putendl and putendlfd

--- ft_putendl.c
+++ ft_putendl.c
@@ -10,4 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_putendl(char const *s)
+{
+	ft_putstr(s);
+	ft_putstr("\n");
+}
