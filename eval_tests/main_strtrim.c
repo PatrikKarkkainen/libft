72e434b2ecf4dec478fafd7cdb1b496741410dc5
Patrik Karkkainen <pkarkkai@student.hive.fi>
2021-11-08 11:41:47 +0200

    Created all files for functions

--- eval_tests/main_strtrim.c
+++ eval_tests/main_strtrim.c
@@ -1,32 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*   main_strtrim.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pkarkkai <pkarkkai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/05 12:57:59 by pkarkkai          #+#    #+#             */
-/*   Updated: 2021/11/08 11:21:28 by pkarkkai         ###   ########.fr       */
+/*   Created: 2021/11/08 11:40:23 by pkarkkai          #+#    #+#             */
+/*   Updated: 2021/11/08 11:40:25 by pkarkkai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 
-void	ft_putnbr_fd(int n, int fd)
-{
-	if (n == INT_MIN)
-	{
-		ft_putstr_fd("-2147483648", fd);
-	}
-	else if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
-	}
-	else if (n > 9)
-	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
-	}
-	else
-		ft_putchar_fd(n + '0', fd);
-}
