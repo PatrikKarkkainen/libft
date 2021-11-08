ba7b4d307e14edf489d834d6e87e19b21631df0d
Patrik Karkkainen <pkarkkai@student.hive.fi>
2021-11-08 12:54:13 +0200

    Added strcmp and strncmp

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,8 +6,22 @@
 /*   By: pkarkkai <pkarkkai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 11:27:36 by pkarkkai          #+#    #+#             */
-/*   Updated: 2021/11/08 11:27:38 by pkarkkai         ###   ########.fr       */
+/*   Updated: 2021/11/08 12:42:00 by pkarkkai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+int	ft_strcmp(const char *str1, const char *str2)
+{
+	int	i;
+
+	i = 0;
+	while (str1[i] && str2[i])
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (str1[i] - str2[i]);
+}
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,8 +6,22 @@
 /*   By: pkarkkai <pkarkkai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 11:27:45 by pkarkkai          #+#    #+#             */
-/*   Updated: 2021/11/08 11:27:47 by pkarkkai         ###   ########.fr       */
+/*   Updated: 2021/11/08 12:51:03 by pkarkkai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+int	ft_strncmp(const char *str1, const char *str2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (str1[i] && str2[i] && i < (n - 1))
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (str1[i] - str2[i]);
+}
