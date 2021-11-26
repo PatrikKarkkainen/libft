0551c44adb0b8e9747e020fdb17ec9890661e769
PatrikKarkkainen <karkkainenpatrik@gmail.com>
2021-11-26 15:44:59 +0200

    Fixed norm error on lstdelone

---
+++ ft_lstdelone.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pkarkkai <pkarkkai@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/26 14:27:14 by pkarkkai          #+#    #+#             */
+/*   Updated: 2021/11/26 15:44:41 by pkarkkai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
+{
+	(*del)((*alst)->content, (*alst)->content_size);
+	free(*alst);
+	*alst = NULL;
+}
